1983576269475710b5fac7c6ec37fb280a5ebf20
Thu Tran 19bit <1959039@itec.hcmus.edu.vn>
2020-11-10 00:11:27 +0700

    save1

--- app/src/main/java/com/group3oop/fetcipe/viewmodels/RecipeListViewModel.java
+++ app/src/main/java/com/group3oop/fetcipe/viewmodels/RecipeListViewModel.java
@@ -1,7 +1,7 @@
 package com.group3oop.fetcipe.viewmodels;
 
-import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.ViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
 
 import com.group3oop.fetcipe.models.Recipe;
 import com.group3oop.fetcipe.repositories.RecipeRepository;
